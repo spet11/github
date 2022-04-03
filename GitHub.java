5bda6ba6b0257bd5fe9b7fac30fb52879040ade8
sanjay patel <nishs1811@gmail.com>
2022-04-03 12:58:04 +0100

    add file

---
+++ GitHub.java
@@ -0,0 +1,12 @@
+package com.github.java;
+
+public class GitHub {
+    public static void main(String[]args){
+        System.out.println("i learn git");
+        System.out.println("i learn also Github");
+    }
+
+}
+
+
+
